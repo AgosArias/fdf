788e81637759bad211cc01f103dfce67e64dd4d5
AgosArias <agosarias99@gmail.com>
2025-10-05 14:23:53 +0200

    add structure and libs in fdf.h

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: agossariass <agossariass@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 13:47:42 by agossariass       #+#    #+#             */
-/*   Updated: 2025/10/04 12:15:45 by agossariass      ###   ########.fr       */
+/*   Updated: 2025/10/05 13:09:28 by agossariass      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,19 @@
 # include <fcntl.h>
 # include <unistd.h>
 # include "get_next_line.h"
+# include <mlx.h>
+# include "libft.h"
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*win;
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
 
 void	ft_error(char *msg);
 void	ft_free_matriz(char **matriz);

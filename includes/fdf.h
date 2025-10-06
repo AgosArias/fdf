07e7a82dac7b3cbe5d0bcdb7506fa0641b23e06e
AgosArias <agosarias99@gmail.com>
2025-10-06 12:05:11 +0200

    create structs and ft

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: agossariass <agossariass@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 13:47:42 by agossariass       #+#    #+#             */
-/*   Updated: 2025/10/05 13:09:28 by agossariass      ###   ########.fr       */
+/*   Updated: 2025/10/06 12:02:05 by agossariass      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include "get_next_line.h"
 # include <mlx.h>
 # include "libft.h"
+# include <stdlib.h>
 
 typedef struct s_data
 {
@@ -32,8 +33,28 @@ typedef struct s_data
 	int		endian;
 }	t_data;
 
+typedef struct s_map
+{
+	int		height;
+	int		width;
+	int		**z;
+}	t_map;
+
+typedef struct s_fdf
+{
+	t_map		map;
+	t_data		img;
+}	t_fdf;
+
 void	ft_error(char *msg);
 void	ft_free_matriz(char **matriz);
 char	**ft_create_matriz(char *str);
+void	ft_create_window(t_data *img);
+void	ft_free_matriz_int(int **matrix);
+int	*ft_fill_row(char *str, int size);
+void	ft_get_wight_height(int fd, int *wight, int *height);
+
+
+
 
 #endif
